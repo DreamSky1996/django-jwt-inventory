7c11edc9d2bcbaa1f075caaa8d9ff5290e8b650b
DreamSky1996 <webhipe@gmail.com>
2022-09-16 05:24:50 +0900

    dockerize

--- inventory/settings.py
+++ inventory/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-66jr0@i2mg#qx$y)dwbd2e_w#=o@g%+kzrron59j7()$st^&)=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '', 
-        'USER': '', 
-        'PASSWORD': '',
-        'HOST': '127.0.0.1', 
+        'NAME': "postgres", 
+        'USER': "postgres", 
+        'PASSWORD': "postgres",
+        'HOST': 'db', 
         'PORT': '5432',
     }
 }
